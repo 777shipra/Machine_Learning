4f39324fa4bb7277d130506deefec2c5ffa1b57f
shipra chauhan <777chauhanshipra@gmail.com>
2018-07-24 20:34:45 +0530

    commit

--- Part 4 - Clustering/Section 24 - K-Means Clustering/K_Means/kmeans.py
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/K_Means/kmeans.py
@@ -24,27 +24,39 @@ y_train = sc_y.fit_transform(y_train)"""
 
 # Using the elbow method to find the optimal number of clusters
 from sklearn.cluster import KMeans
-wcss = []
+wcss = []#within cluster sum of squares
 for i in range(1, 11):
-    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    #max_iter is the max no of iterations to be placed during k means is running
+    #n_init tells how many times initialization to be done for clusters
+    #300 and 10 are the default values 
+    kmeans = KMeans(n_clusters = i, init = 'k-means++',max_iter=300,n_init=10, random_state = 42)#init is for resolving the random initialisation trap
     kmeans.fit(X)
-    wcss.append(kmeans.inertia_)
+    wcss.append(kmeans.inertia_)#intertia is a special method for appending in wcss
 plt.plot(range(1, 11), wcss)
 plt.title('The Elbow Method')
 plt.xlabel('Number of clusters')
 plt.ylabel('WCSS')
 plt.show()
+#the above code shows us different values corresponding to different k 
+#from which we found out that 5 if the optimal point 
 
+
+#so again calling k means with optimal cluster 
 # Fitting K-Means to the dataset
-kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 42)
+kmeans = KMeans(n_clusters = 5, init = 'k-means++', max_iter=300,n_init=10,random_state = 42)
 y_kmeans = kmeans.fit_predict(X)
 
 # Visualising the clusters
+#y_means==0 for cluster 1 as counting begin from 0 to 4 
+#y_means==0,0 is for x axis and 1 for y axis 
+#s is the size of the points 
 plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
 plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
 plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
 plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
 plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+
+#this is to plot the centroids with bigger size 
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
 plt.title('Clusters of customers')
 plt.xlabel('Annual Income (k$)')
