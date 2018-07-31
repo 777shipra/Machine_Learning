e98589f34378ba24fa4be0e762e61058ff96f807
shipra chauhan <777chauhanshipra@gmail.com>
2018-07-31 11:33:03 +0530

    NLP

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural_Language_Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural_Language_Processing/natural_language_processing.py
@@ -22,21 +22,38 @@ import re #used to clean the text
 import nltk
 nltk.download('stopwords')#it is the list containing the word which we do not want in the review
 from nltk.corpus import stopwords#to import stopwords to spyder
-from nltk.stem.porter import PorterStemmer
-corpus = []
+from nltk.stem.porter import PorterStemmer#for importing stemming
+corpus = []#list containing 1000 clean reviews 
+#for all the 1000 reviews in the dataset
 for i in range(0, 1000):
     review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])# for having only letters in the review a-zA-Z , replacing them with space inorder to stick the words if we remove some signs and numbers , the index 
     review = review.lower()#make all lower case letters in review
     review = review.split()#spiliting the string into list of strings containing each word seperated
-    ps = PorterStemmer()
-    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
-    review = ' '.join(review)
+    ps = PorterStemmer()#creating an object before appling stemming on the review
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]#applying stemming and stopwards in english on the reviews 
+    review = ' '.join(review) #joining back the list of strings and seperate them with space
     corpus.append(review)
+'''
 
+
+Sparsity is a property of our data which we cannot change. We can use our knowledge about it to apply methods which take it into account and as such more efficient on sparse data.
+
+A corpus is a object in our programming environment for containing text data. It is convenient to keep it in a compact form and to retrieve what we want quickly.
+
+'''
 # Creating the Bag of Words model
-from sklearn.feature_extraction.text import CountVectorizer
+''' the bag of words will help us in giving one word to each review (unique) and making it a column 
+that is why we cleaned the reviews before applying this 
+a table will be created containing
+rows - corpus
+columns will be unique words
+that will create a sparx matrix through the process of tokenization leading to scarcity and in ML we try to reduce sparcity as much as possible
+why ?
+-to predict positive or negative by training the machine  
+here we are doing nothing as classification  '''
+from sklearn.feature_extraction.text import CountVectorizer #for tokenization as manually it will take alot of time 
 cv = CountVectorizer(max_features = 1500)
-X = cv.fit_transform(corpus).toarray()
+X = cv.fit_transform(corpus).toarray()#this step will create the sparx matrix , X is the matrix of features
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
